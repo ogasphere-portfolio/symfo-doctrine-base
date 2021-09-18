99cbd97a39b67aaf482afb622b66a2344b6dfe67
Olivier <olivier@ogasphere.com>
2021-09-18 10:39:45 +0200

    crud des posts + formulaire

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,19 +2,94 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Post;
+use App\Form\PostFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
+   
     /**
-     * @Route("/", name="post")
+     * @Route("/add-post", name="add_post")
      */
-    public function index(): Response
+    public function addProduct(Request $request): Response
     {
-        return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+        $post = new Post();
+        $form = $this->createForm(PostFormType::class, $post);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($post);
+            $entityManager->flush();
+        }
+
+        return $this->render("post/post-form.html.twig", [
+            "form_title" => "Ajouter un post",
+            "form_post" => $form->createView(),
         ]);
+    }
+
+    /**
+    * @Route("/posts", name="posts")
+    */
+    public function posts()
+    {
+        $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
+
+        return $this->render('post/posts.html.twig', [
+            "posts" => $posts,
+        ]);
+    }
+
+    /**
+    * @Route("/post/{id}", name="post")
+    */
+    public function post(int $id): Response
+    {
+        $post = $this->getDoctrine()->getRepository(Post::class)->find($id);
+
+        return $this->render("post/post.html.twig", [
+            "post" => $post,
+        ]);
+    }
+
+    /**
+    * @Route("/modify-post/{id}", name="modify_post")
+    */
+    public function modifyPost(Request $request, int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $post = $entityManager->getRepository(Post::class)->find($id);
+        $form = $this->createForm(PostFormType::class, $post);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->flush();
+        }
+
+        return $this->render("post/post-form.html.twig", [
+            "form_title" => "Modifier un article",
+            "form_post" => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/delete-post/{id}", name="delete_post")
+     */
+    public function deletePost(int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $post = $entityManager->getRepository(Post::class)->find($id);
+        $entityManager->remove($post);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("posts");
     }
 }
--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -12,6 +12,7 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Post[]    findAll()
  * @method Post[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
+
 class PostRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
